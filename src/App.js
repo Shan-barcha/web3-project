9739fc4e866c8ee45e882e66ea14d710e1f25aaa
Shan-barcha <shanbarcha11@gmil.com>
2023-09-12 23:21:29 +0500

    import lottery contract

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import Web3 from "web3";
- 
-window.ethereum.request({ method: "eth_requestAccounts" });
- 
-const web3 = new Web3(window.ethereum);
- 
-export default web3;
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import web3 from "./web3";
+
+class App extends React.Component {
+
+  render() {
+    console.log(web3);
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
